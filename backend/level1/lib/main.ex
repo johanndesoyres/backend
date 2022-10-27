67cf892c9f5af5e26d23e66dd5ed09073980cad8
Johann de Soyres <johann.desoyres@ulysse.com>
2022-10-27 17:54:57 +0200

    post home test : commit forgotten files from level1 folder

--- backend/level1/lib/main.ex
+++ backend/level1/lib/main.ex
@@ -1,21 +1,22 @@
 defmodule Main do
   @doc """
-  Parser for the input json file.
+  Reader for the input json file.
 
   ## Examples
 
-      iex> Main.parser("data/input.json")
-      %{
-        "carriers" => [
-          %{"code" => "colissimo", "delivery_promise" => 3},
-          %{"code" => "ups", "delivery_promise" => 2}
+      iex> Main.read_file("data/input.json")
+     {:ok,
+        %{
+        carriers: [
+          %{code: "colissimo", delivery_promise: 3},
+          %{code: "ups", delivery_promise: 2}
         ],
-        "packages" => [
-          %{"carrier" => "colissimo", "id" => 1, "shipping_date" => "2018-05-01"},
-          %{"carrier" => "ups", "id" => 2, "shipping_date" => "2018-05-14"},
-          %{"carrier" => "colissimo", "id" => 3, "shipping_date" => "2018-06-10"}
+        packages: [
+          %{carrier: "colissimo", id: 1, shipping_date: "2018-05-01"},
+          %{carrier: "ups", id: 2, shipping_date: "2018-05-14"},
+          %{carrier: "colissimo", id: 3, shipping_date: "2018-06-10"}
         ]
-      }
+      }}
   """
   def read_file(input_name) do
     input_name
@@ -23,10 +24,21 @@ defmodule Main do
     |> Poison.decode(keys: :atoms)
   end
 
+  @doc """
+  Writer for the output file.
+
+  ## Examples
+
+      iex> Main.write_file("data/input.json")
+     :ok
+  """
   def write_file(content, output_name) do
     :ok = File.write(output_name, Poison.encode!(content), [:binary])
   end
 
+  @doc """
+  Compute the expected delivery date for a specific package.
+  """
   def expected_delivery_date(package, delivery_promises) do
     package.shipping_date
     |> Date.from_iso8601!()
@@ -34,6 +46,10 @@ defmodule Main do
     |> Date.to_string()
   end
 
+  @doc """
+  Compute the expected delivery dates for all packages from the input json
+  file and write the result in the output json file.
+  """
   def compute_expected_delivery_dates do
     {:ok, content} = read_file("data/input.json")
 
@@ -54,6 +70,7 @@ defmodule Main do
           | acc
         ]
       end)
+      |> Enum.reverse()
 
     :ok = write_file(%{deliveries: expected_delivery_dates}, "data/output.json")
   end
--- backend/level1/test/level1_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Level1Test do
-  use ExUnit.Case
-  doctest Level1
-
-  test "greets the world" do
-    assert Level1.hello() == :world
-  end
-end
